1bbc0b2633242b847ea337881c0fb2d15d851017
taoqf <tao_qf@163.com>
2020-03-16 13:15:16 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,16 @@
+import global from '@mmstudio/global';
 
-export default async function aw22(param1: string, param2: string) {
+/**
+ * 获取图片地址
+ * @param field_name 数据中图片地址的字段名
+ * @param data 数据
+ * ```ts
+ */
+export default function generate_file_url(field_name: string, data: { [field_name: string]: number | string | boolean }[]) {
+	return data.map((it) => {
+		return {
+			...it,
+			[field_name]: `${global('host', '.')}/fsweb/getfile?id=${it[field_name]}`
+		};
+	});
 }
--- tests/test.js
+++ tests/test.js
@@ -1,9 +1,34 @@
 import { expect } from 'chai';
 import t from '../dist/index';
 
-describe('xxx', () => {
-	it('xxx', () => {
-		t();
-		expect(1).not.eq(2);
+const data = [{
+	name: 'foo',
+	id: 'id1'
+}, {
+	name: 'bar',
+	id: 'id2'
+}];
+
+describe('文件id拼接url', () => {
+	it('部署', () => {
+		const v = t('id', data);
+		expect(v).deep.eq([{
+			name: 'foo',
+			id: './fsweb/getfile?id=id1'
+		}, {
+			name: 'bar',
+			id: './fsweb/getfile?id=id2'
+		}]);
+	});
+	it('开发', () => {
+		window.host = 'http://127.0.0.1:8889'
+		const v = t('id', data);
+		expect(v).deep.eq([{
+			name: 'foo',
+			id: 'http://127.0.0.1:8889/fsweb/getfile?id=id1'
+		}, {
+			name: 'bar',
+			id: 'http://127.0.0.1:8889/fsweb/getfile?id=id2'
+		}]);
 	});
 });
